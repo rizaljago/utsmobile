20af6fb782b17961ee1c598a23a8fca9c8c3d8cc
virgiawanind <54799831+virgiawanind@users.noreply.github.com>
2020-12-31 11:56:56 +0700

    Update beritapage.dart
    update

--- lib/screens/beritapage.dart
+++ lib/screens/beritapage.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:love_yourselfid/screens/berita/newspagetwo.dart';
 import 'akunpage.dart';
-import 'loginpage.dart';
-import 'akunpage.dart';
-import 'loginpage.dart';
-import 'akunpage.dart';
-import 'beritapage.dart';
 import 'eventpage.dart';
-import 'loginpage.dart';
 import 'artikelpage.dart';
 import 'homepage.dart';
+import 'package:love_yourselfid/screens/berita/newspageone.dart';
 import 'package:love_yourselfid/utils/customclipper.dart';
 
 
@@ -26,6 +22,7 @@ class _BeritaPageState extends State<BeritaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink.shade100,
+      resizeToAvoidBottomPadding: false,
       drawer: Drawer(
         child: Container(
           color: Colors.pink.shade100,
@@ -181,8 +178,96 @@ class _BeritaPageState extends State<BeritaPage> {
         ),
       ),
       body: Container(
-        margin: EdgeInsets.all(20),
+        margin: EdgeInsets.only(left: 10, top: 0, bottom: 0, right: 10),
         color: Colors.pink.shade100,
+        child: Column(
+            children: <Widget>[
+              Card(
+                elevation: 15,
+                shape: (
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    )
+                ),
+                color: Colors.pink.shade50,
+                margin: EdgeInsets.only(left: 10, top: 20, bottom: 0, right: 10),
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: const Text('ABG 15 Tahun di Surabaya Alami Pelecehan', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
+                      subtitle: Text(
+                        'Surabaya - Jangan mudah tergiur tawaran orang tak dikenal. Seorang ABG berusia 15 tahun..',
+                        style: TextStyle(color: Colors.black,
+                            fontSize: 16),
+                      ),
+                    ),
+                    Image.asset('assets/card3.png', scale: 1, height: 150,),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.start,
+                      children: [
+                        FlatButton(
+                          textColor: const Color(0xFF6200EE),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => NewsPageOne()),
+                            );
+                          },
+                          child: const Text('Baca Selengkapnya'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                elevation: 15,
+                shape: (
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    )
+                ),
+                color: Colors.pink.shade50,
+                margin: EdgeInsets.only(left: 10, top: 20, bottom: 0, right: 10),
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: const Text('Pelaku Pelecehan Seksual Anak Didominasi Keluarga Dekat, Waspada!', style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      )),
+                      subtitle: Text(
+                        'BLITAR - Pelaku pelecehan seksual sepanjang tahun 2020 di...',
+                        style: TextStyle(color: Colors.black,
+                            fontSize: 16),
+                      ),
+                    ),
+                    Image.asset('assets/card4.png', scale: 1, height: 150,),
+                    ButtonBar(
+                      alignment: MainAxisAlignment.start,
+                      children: [
+                        FlatButton(
+                          textColor: const Color(0xFF6200EE),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => NewsPageTwo()),
+                            );
+                          },
+                          child: const Text('Baca Selengkapnya'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ]
+        ),
       ),
 
     );
