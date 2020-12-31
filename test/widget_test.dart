c090c0dd94a66b0e038c9bf7418b88eb9109580c
virgiawanind <54799831+virgiawanind@users.noreply.github.com>
2020-12-31 12:20:07 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:loveyourselffix/main.dart';
+import 'package:loveyourselfid/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
